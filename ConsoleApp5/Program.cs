1faf9d65a51e23ed1632fd613b80aaac5a866bf4
Lucas Spanhol <lucastspanhol@hotmail.com>
2018-08-29 10:03:43 -0300

    segundo commit

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace ConsoleApp5 {
     class Program {
         static void Main(string[] args) {
-            Console.Write("Teste git");
+            Console.WriteLine("Teste git");
+            Console.WriteLine("Teste git 2");
         }
     }
 }
